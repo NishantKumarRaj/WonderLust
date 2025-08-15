748919f9919c3150b8370b18e14461be71f57351
NishantKumarRaj <nishantkrraj111@gmail.com>
2025-08-15 14:35:37 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,121 @@
+if (process.env.NODE_ENV != "production") {
+    require('dotenv').config();
+}
+
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+const path = require("path");
+const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
+const ExpressError = require('./utils/ExpressError');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/userModel');
+
+
+const listingsRouter = require('./routes/listingRouter');
+const reviewsRouter = require('./routes/reviewRouter');
+const userRouter = require('./routes/userRouter');
+
+
+const dbUrl = process.env.ATLAS_DB_URL;
+
+main().then(() => {
+    console.log("Connect to Wanderlust DB");
+}).catch((err) => {
+    console.log(err);
+});
+
+async function main() {
+    await mongoose.connect(dbUrl);
+}
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, "/public/css")));
+app.use(express.static(path.join(__dirname, "/public/js")));
+
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    client: mongoose.connection.getClient(),
+    crypto: {
+        secret: process.env.SECRET,
+    },
+    touchAfter: 24 * 3600,
+});
+
+store.on("error", () => {
+    console.log("ERROR in Mongo Session Store");
+})
+
+
+const sessionOptions = {
+    store,
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+    },
+};
+
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    res.locals.currUser = req.user;
+    next();
+});
+
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
+
+app.use("/listings", listingsRouter);
+app.use("/listings/:id/reviews", reviewsRouter);
+app.use("/", userRouter);
+
+
+// Page Not Found Error
+app.all("*", (req, res, next) => {
+    next(new ExpressError(404, "Page Not Found"));
+})
+
+
+// ERROR HANDLING MIDDLEWARE   
+app.use((err, req, res, next) => {
+    let { statusCode = 500, message = "Some error occurred!" } = err;
+    res.status(statusCode).render("error.ejs", { err });
+    // res.status(statusCode).send(message);
+    // re
+});
+
+
+app.listen(3000, () => {
+    console.log("server is on at 3000");
+});
